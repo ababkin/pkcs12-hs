67e014513e299f069d22a498ab5dbd1ab1bab403
Takashi Kawachi <tkawachi@gmail.com>
2016-03-03 22:12:59 +0900

    Used QuickCheck

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,11 @@
-import Test.Hspec
 import Crypto.Pkcs12.BmpString (
   BmpString(..),
   encode,
   decode
   )
 import Data.ByteString
+import Test.Hspec
+import Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
@@ -16,6 +17,5 @@ main = hspec $ do
           [0x00, 0x42, 0x00, 0x65, 0x00, 0x61, 0x00, 0x76, 0x00, 0x69, 0x00, 0x73, 0x00, 0x00]
 
     describe "decode" $ do
-      it "decodes an encoded BmpString" $ do
-        let s = "Foo abc"
-        decode (encode s) `shouldBe` Right s
+      it "decodes an encoded BmpString" $ property $
+        \s -> decode (encode s) `shouldBe` Right s
