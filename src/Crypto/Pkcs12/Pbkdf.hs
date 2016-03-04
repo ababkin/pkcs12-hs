9b11f4fb04491b20286c240353e3deb59947b037
Takashi Kawachi <tkawachi@gmail.com>
2016-03-04 23:13:06 +0900

    Added a pbkdf test case

--- src/Crypto/Pkcs12/Pbkdf.hs
+++ src/Crypto/Pkcs12/Pbkdf.hs
@@ -2,8 +2,9 @@ module Crypto.Pkcs12.Pbkdf (
   pbkdf
   ) where
 
+import           Crypto.Pkcs12.BmpString (BmpString)
 import qualified Data.ByteString as B
-import Data.Word (Word8)
+import           Data.Word (Word8)
 
 -- https://tools.ietf.org/html/rfc7292#appendix-B.2
 -- https://github.com/golang/crypto/blob/master/pkcs12/pbkdf.go
@@ -12,7 +13,7 @@ pbkdf :: (B.ByteString -> B.ByteString) ->
          Int ->
          Int ->
          B.ByteString ->
-         B.ByteString ->
+         BmpString ->
          Int ->
          Word8 ->
          Int ->
